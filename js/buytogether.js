493f9d6d1bc697e098e668a9a2a25c4306ead8d2
freiqu <f.mauelshagen@mailbox.org>
2019-05-25 16:31:11 +0200

    inserat listen speicher

--- js/buytogether.js
+++ js/buytogether.js
@@ -1,17 +1,31 @@
+var auftragid = 0
+
+
 function helloworld(){
   console.log("hallo welt");
 }
-function saveEinkaeufer(){
-  var name = "nameEinkaeufer"
-  var namedata = document.getElementById('nameEinkaeufer').value
-  window.localStorage.setItem(name,namedata)
-}
-function saveFaulerTyp(){
-  var name = "nameFaulerTyp"
-  var namedata = document.getElementById('nameFaulerTyp').value
-  window.localStorage.setItem(name,namedata)
+function saveAuftrag(){
+  var inserat = {}
+  inserat.nameEinkaeufer = document.getElementById("nameAuftrag").value
+  inserat.zeitAuftrag = document.getElementById("zeitAuftrag").value
+  inserat.ladenAuftrag = document.getElementById("ladenAuftrag").value
+  window.localStorage.setItem(auftragid,inserat)
+  auftragid = auftragid + 1
 }
 function read(){
   var key = "hi"
   console.log(window.localStorage.getItem(key))
+}
+
+function allStorage() {
+
+    var values = [],
+        keys = Object.keys(localStorage),
+        i = keys.length;
+
+    while ( i-- ) {
+        values.push( localStorage.getItem(keys[i]) );
+    }
+
+    return values;
 }
