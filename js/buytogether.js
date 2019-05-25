8d6e20657afa8809fcd413a07bfa6566dcfaab48
Alexander Diewald <alexander.diewald@teckids.org>
2019-05-25 15:43:41 +0200

    beide buttons

--- js/buytogether.js
+++ js/buytogether.js
@@ -1,11 +1,19 @@
 function helloworld(){
   console.log("hallo welt");
 }
-function save(){
+function saveEinkaeufer(){
   var key = "hi"
   var data = "hello world"
   var name = "name"
   var namedata = document.getElementById('nameEinkaeufer').value
+  window.localStorage.setItem(key,data)
+  window.localStorage.setItem(name,namedata)
+}
+function saveFaulerTyp(){
+  var key = "hi"
+  var data = "hello world"
+  var name = "name"
+  var namedata = document.getElementById('nameFaulerTyp').value
   window.localStorage.setItem(key,data)
   window.localStorage.setItem(name,namedata)
 }
