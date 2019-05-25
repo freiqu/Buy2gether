4b602b168a50d99e666a58c843b8ef9ba16795f1
freiqu <f.mauelshagen@mailbox.org>
2019-05-25 17:40:17 +0200

    speichern, auslesen und anderes

--- js/buytogether.js
+++ js/buytogether.js
@@ -1,17 +1,29 @@
 var auftragid = 0
 
 
+
 function helloworld(){
   console.log("hallo welt");
 }
 function saveAuftrag(){
   var inserat = {}
-  inserat.nameEinkaeufer = document.getElementById("nameAuftrag").value
+  inserat.nameEinkaeufer = document.getElementById("nameEinkaeufer").value
   inserat.zeitAuftrag = document.getElementById("zeitAuftrag").value
   inserat.ladenAuftrag = document.getElementById("ladenAuftrag").value
   window.localStorage.setItem(auftragid,inserat)
   auftragid = auftragid + 1
+  console.log("jetzt würde diese Liste an den Host geschickt werden:")
+  console.log(inserat)
 }
+
+function sendeAuftrag(){
+  var inserat = {}
+  inserat.nameAuftraggeber = document.getElementById("nameAuftraggeber").value
+  inserat.einkaufslisteAuftraggeber = document.getElementById("einkaufslisteAuftraggeber").value
+  console.log("jetzt würde diese Liste an den Host geschickt werden:")
+  console.log(inserat)
+}
+
 function read(){
   var key = "hi"
   console.log(window.localStorage.getItem(key))
