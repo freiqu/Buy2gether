7586fd21ecee7430a569702c44d7c1a07feb28cf
freiqu <f.mauelshagen@mailbox.org>
2019-05-25 14:55:30 +0200

    buytogether.js hinzugefügt

---
+++ js/buytogether.js
@@ -0,0 +1,15 @@
+function helloworld(){
+  console.log("hallo welt");
+}
+function save(){
+  var key = "hi"
+  var data = "hello world"
+  var name = "name"
+  var namedata = document.getElementById('name').value
+  window.localStorage.setItem(key,data)
+  window.localStorage.setItem(name,namedata)
+}
+function read(){
+  var key = "hi"
+  console.log(window.localStorage.getItem(key))
+}
